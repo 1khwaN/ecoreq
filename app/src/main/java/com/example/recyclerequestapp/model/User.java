8b1bbed43423041e95c6fb42ec9ee2ee3b31772d
hakimiirewan <108714167+hakimiirewan@users.noreply.github.com>
2025-07-17 15:28:59 +0800

    Update User.java

--- app/src/main/java/com/example/recyclerequestapp/model/User.java
+++ app/src/main/java/com/example/recyclerequestapp/model/User.java
@@ -1,18 +1,89 @@
 package com.example.recyclerequestapp.model;
 
 public class User {
-    private String user_id;
-    private String user_name;
-    private String user_email;
-    private String user_phoneNum;
-    private String role;
-    private String token; // if your API returns a token
 
-    // Getters
-    public String getUser_id() { return user_id; }
-    public String getUser_name() { return user_name; }
-    public String getUser_email() { return user_email; }
-    public String getUser_phoneNum() { return user_phoneNum; }
-    public String getRole() { return role; }
-    public String getToken() { return token; }
+    // represent a record in users tables
+    private int id;
+    private String email;
+    private String username;
+    private String password;
+    private String token;
+    private String lease;
+    private String role;
+    private int is_active;
+    private String secret;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getLease() {
+        return lease;
+    }
+
+    public void setLease(String lease) {
+        this.lease = lease;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getIs_active() {
+        return is_active;
+    }
+
+    public void setIs_active(int is_active) {
+        this.is_active = is_active;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+
 }
