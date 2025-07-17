56c68f7eb58f4ef5d5a4ef8cec2bd7c8ada2f035
1khwaN <ikhwanhakimi03@gmail.com>
2025-07-18 01:21:43 +0800

    requestService changes

--- app/src/main/java/com/example/recyclerequestapp/remote/RequestService.java
+++ app/src/main/java/com/example/recyclerequestapp/remote/RequestService.java
@@ -6,10 +6,22 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface RequestService {
 
+    // Admin: Get all requests
     @FormUrlEncoded
     @GET("requests") // Assuming your pRESTige endpoint for requests is /requests
     Call<List<Request>> getAllRequests();
+
+
+    // User: Get requests for a specific user
+    // Option 1: Using query parameter (common with pRESTige for filtering)
+    @GET("requests")
+    Call<List<Request>> getMyRequests(@Query("user_id") int userId);
+
+    // Option 2: If your backend has a dedicated endpoint like /users/{id}/requests
+    // @GET("users/{id}/requests")
+    // Call<List<Request>> getMyRequests(@Path("id") int userId);
 }
